97f0a59b6f097945874658652c263ed253b3d593
RoyalTra <RoyalTra31@gmail.com>
2017-12-07 05:59:52 -0600

    Update CSS Style

--- volunteerlogging/app/assets/javascripts/application.js
+++ volunteerlogging/app/assets/javascripts/application.js
@@ -19,3 +19,9 @@
 //= require Chart.min
 //= require moment
 //= require fullcalendar
+//= require bootstrap-wysihtml5
+//= require bootstrap-wysihtml5/locales
+//= require bootstrap-wysihtml5/wysihtml5x-toolbar.js
+//= require bootstrap-wysihtml5/handlebars.runtime.min.js
+
+//= require bootstrap-wysihtml5/minimum
